0bc59f6c990e4a9bb70ab5720bd782aed6cdcda9
Nur Islam <nur.islam@pathao.com>
2021-12-18 02:55:27 +0600

    Add consumer worker to consume message

--- cmd/worker.go
+++ cmd/worker.go
@@ -1,1 +1,53 @@
 package cmd
+
+import (
+	"github.com/nurislam03/postoffice/config"
+	"github.com/nurislam03/postoffice/conn"
+	cwrkr "github.com/nurislam03/postoffice/worker"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+var workerCmd = &cobra.Command{
+	Use:   "worker",
+	Short: "Start worker",
+	Long:  `Start the worker server`,
+	Run:   worker,
+}
+
+func init() {
+	RootCmd.AddCommand(workerCmd)
+}
+
+func worker(cmd *cobra.Command, args []string) {
+	count, err := strconv.Atoi(args[0])
+	if err != nil || count < 1 {
+		logrus.Error("concurrency should be a positive number", err)
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGKILL, syscall.SIGINT, syscall.SIGQUIT)
+
+	go startWorker(count)
+
+	<-stop
+	log.Println("Worker server shutting down...")
+
+	time.Sleep(5 * time.Second)
+}
+
+func startWorker(count int) {
+	cfg := config.NewConfig()
+
+	//connection
+	amqpServer := conn.AMQPServer(cfg.AMQP)
+	wrkr := cwrkr.NewWorker(amqpServer)
+
+	wrkr.Run(count)
+}
--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,75 @@
 package worker
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type Worker struct {
+	amqpServer *amqp.Connection
+}
+
+//NewWorker ...
+func NewWorker(w *amqp.Connection) *Worker {
+	return &Worker{
+		amqpServer: w,
+	}
+}
+
+func (w *Worker) Run(count int) {
+	name := "get-status"
+
+	ch, err := w.amqpServer.Channel()
+	if err != nil {
+		logrus.Error("Failed to open RabbitMQ channel", err)
+	}
+
+	defer ch.Close()
+
+	_, err = ch.QueueDeclare(
+		name,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		logrus.Error("Failed to declare queue", err)
+	}
+
+	msgs, err := ch.Consume(
+		name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		logrus.Error("Failed to register a consumer", err)
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGKILL, syscall.SIGINT, syscall.SIGQUIT)
+
+	for i := 1; i <= count; i++ {
+		go func(id int) {
+			logrus.Info("Starting worker: ", id)
+			for m := range msgs {
+				logrus.Info("Received a message: ",  string(m.Body))
+				//Todo : database write
+			}
+		}(i)
+	}
+
+	logrus.Info(" [*] Waiting for a message. To exit press CTRL+C")
+
+	<-stop
+}
